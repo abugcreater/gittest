8c01b88196dbdcbe1bd9825e0c7823859e63dffa
fk123 <1572020281@qq.com>
2018-07-06 09:29:33 +0800

    增加堆溢出测试方法

--- src/com/fk/HelloWorld.java
+++ src/com/fk/HelloWorld.java
@@ -1,9 +1,27 @@
 package com.fk;
 
+import java.util.ArrayList;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.testOutMemary();
+
         System.out.println("Hello World");
     }
+
+    public void testOutMemary(){
+
+        //测试堆溢出状态
+        ArrayList list=new ArrayList();
+        while(true)
+        {
+            list.add(new HelloWorld());
+
+        }
+    }
+
+
 
 }
